755cdf0f70fc480bcf97f7f2f2ded43a4496950a
DatReki <TimoHalofan@outlook.com>
2022-03-04 03:14:59 +0100

    Initial commit

---
+++ RunDiskPartCommands/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunDiskPartCommands
+{
+    internal static class Extensions
+    {
+        internal static void RunCommand(this Process process, string command)
+        {
+            process.StandardInput.WriteLine(command);
+        }
+    }
+}
---
+++ RunDiskPartCommands/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace RunDiskPartCommands
+{
+    class Program
+    {
+        internal static string s_workingDirectory
+        {
+            get
+            {
+                return Environment.SystemDirectory;
+            }
+        }
+
+        internal static string s_file
+        {
+            get
+            {
+                return Path.Combine(s_workingDirectory, "diskpart.exe");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process process = new Process()
+                {
+                    StartInfo = new()
+                    {
+                        WorkingDirectory = s_workingDirectory,
+                        FileName = s_file,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = false,
+                        UseShellExecute = false
+                    }
+                };
+                process.Start();
+#if DEBUG
+                process.RunCommand("list disk");
+#endif
+                process.RunCommand("select disk 4");
+                process.RunCommand("attributes disk clear readonly");
+                process.RunCommand("exit");
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
+                process.WaitForExit();
+#if DEBUG
+                Console.WriteLine(process.StandardOutput.ReadToEnd());
+                Console.ReadLine();
+#endif
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong!\nError: {ex.ToString()}\nPress enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
